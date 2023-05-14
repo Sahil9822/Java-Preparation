c702905ed2f78bc2d6d2ccb40b8b0e2f35ae95cc
Sahil Pawale <97694477+Sahil9822@users.noreply.github.com>
2023-05-14 22:42:46 +0530

    Add files via upload

---
+++ IfElse.java
@@ -0,0 +1,15 @@
+public class IfElse 
+{
+	public static void main(String[] args) 
+	{
+		int num1 = 8;
+		if(num1 % 2 ==0) 
+		{
+			System.out.println("Even");
+		}
+		else 
+		{
+			System.out.println("Odd");
+		}
+	}
+}
