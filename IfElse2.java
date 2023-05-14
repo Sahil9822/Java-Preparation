515ea4a32efc19531726b79d5c4d0157c8ffbcda
Sahil Pawale <97694477+Sahil9822@users.noreply.github.com>
2023-05-14 22:46:03 +0530

    Add files via upload

---
+++ IfElse2.java
@@ -0,0 +1,25 @@
+
+public class IfElse2 
+{
+	public static void main(String[] args) 
+	{
+		int a, b, c;
+		a = 4;
+		b = 13;
+		c = 8;
+		
+		if(a > b && a > c) 
+		{
+			System.out.println("A");
+		}
+		else if(b > c) 
+		{
+			System.out.println("B");
+		}
+		else 
+		{
+			System.out.println("C");
+		}
+	}
+
+}
