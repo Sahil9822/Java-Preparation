76aa9ffe2fe084821e822611ce7032426ed70be1
Sahil Pawale <97694477+Sahil9822@users.noreply.github.com>
2023-05-15 11:47:05 +0530

    Add files via upload

---
+++ SwitchCase.java
@@ -0,0 +1,24 @@
+public class SwitchCase 
+{
+	public static void main(String[] args) 
+	{
+		int i = 7;
+		switch(i) 
+		{
+		case 0:
+			System.out.println("Zero");
+			break;
+		case 1:
+			System.out.println("One");
+			break;
+		case 7:
+			System.out.println("Seven");
+			break;
+		case 9:
+			System.out.println("Nine");
+			break;
+		default:
+			System.out.println("Some Number");
+		}
+	}
+}
