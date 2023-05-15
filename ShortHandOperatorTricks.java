c1695f8cfd6a24fe322b5762ad0a2b9a7a87beae
Sahil Pawale <97694477+Sahil9822@users.noreply.github.com>
2023-05-15 11:46:28 +0530

    Add files via upload

---
+++ ShortHandOperatorTricks.java
@@ -0,0 +1,18 @@
+public class ShortHandOperatorTricks 
+{
+	public static void main(String[] args) 
+	{
+		int i = 5;
+		int j = 10;
+		int k = 15;
+		int l = 20;
+		i += 2;
+		j -= 2;
+		k *= 2;
+		l /= 2;
+		System.out.println(i);
+		System.out.println(j);
+		System.out.println(k);
+		System.out.println(l);
+	}
+}
