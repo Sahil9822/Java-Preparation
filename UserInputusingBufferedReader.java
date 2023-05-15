3178f1a226a26895b15cf855ccc8ad6f0156f574
Sahil Pawale <97694477+Sahil9822@users.noreply.github.com>
2023-05-15 11:48:37 +0530

    Add files via upload

---
+++ UserInputusingBufferedReader.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class UserInputusingBufferedReader 
+{
+
+	public static void main(String[] args) throws Exception
+	{
+		System.out.println("Enter the number");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		System.out.println(n);
+
+	}
+
+}
