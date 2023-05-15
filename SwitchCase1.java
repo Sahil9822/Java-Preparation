e58f5a04596c80b2f6de11585a7a5d742a325128
Sahil Pawale <97694477+Sahil9822@users.noreply.github.com>
2023-05-15 11:47:30 +0530

    Add files via upload

---
+++ SwitchCase1.java
@@ -0,0 +1,17 @@
+
+public class SwitchCase1 
+{
+	public static void main(String[] args) 
+	{
+		String s = "Hi";
+		
+		switch(s) 
+		{
+		case "Hi":
+			System.out.println("Hi");
+			break;
+		case "Hello":
+			System.out.println("Hello");
+		}
+	}
+}
